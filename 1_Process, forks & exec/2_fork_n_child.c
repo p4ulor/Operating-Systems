6ff59a90f50a0a09c560f06646250f78967bd655
p4ulor <A44873@alunos.isel.pt>
2023-09-19 19:54:05 +0100

    7

--- 1_Process, forks & exec/2_fork_n_child.c
+++ 1_Process, forks & exec/2_fork_n_child.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <unistd.h>
 #include <sys/types.h> //cuz of pid_t
+#include <sys/wait.h> //cuz of wait() https://man7.org/linux/man-pages/man2/wait.2.html
 
 //gcc 2_fork_n_child.c
 
@@ -13,14 +14,23 @@ int main() {
 	
 	pid_t pid = fork(); //pid==0 for the Child, pid==Child_PID for the Parent
 	if (pid == 0) {
-        printf("CHILD: pid=%d; Parent PID=%d. And I changed value\n", getpid(), getppid());
+        printf("CHILD: pid=%d; Parent PID=%d. Value=%d And I added 10 to value\n", getpid(), getppid(), value);
         value += 10;
+        return value;
     }
     else if (pid == -1) puts("Out of memory O.O ?");
 	else {
         amIParent = 1;
         sleep(2);
         printf("PARENT: pid=%d; Child PID=%d\n", getpid(), pid);
+
+        //wait(NULL); //wait for any but only 1 child to end
+        pid_t child_pid;
+        int childReturn = 0;
+        while ((child_pid = wait(&childReturn)) > 0); // this way, the father waits for all the child processes
+        if(WIFEXITED(childReturn)){
+            printf("Last child returned %d\n", WEXITSTATUS(childReturn)); //WEXITSTATUS converts the returned value into a valid one. This is because the OG returned value is bundled w/ other info, and WEXITSTATUS extracts what's our actual return value by the process
+        }
     }
 
     char* process = amIParent ? "parent" : "child";
